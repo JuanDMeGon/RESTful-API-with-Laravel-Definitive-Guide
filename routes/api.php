339914af21584b7437789bbbc4f464b1a4eb58ff
JuanDMeGon <juandmegon@gmail.com>
2017-02-14 15:14:48 -0500

    Understanding the Routes on Laravel

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
