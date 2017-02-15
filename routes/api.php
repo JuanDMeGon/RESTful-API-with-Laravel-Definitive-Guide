7a89b43a55891a448134028a739104796e9b8767
JuanDMeGon <juandmegon@gmail.com>
2017-02-15 01:30:49 -0500

    Creating the Initial Resource Routes for The Controllers

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,33 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+/**
+ * Buyers
+ */
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+/**
+ * Categories
+ */
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+/**
+ * Products
+ */
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+/**
+ * Sellers
+ */
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+/**
+ * Transactions
+ */
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+/**
+ * Users
+ */
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
