3b1c650604bd28d34dacee3e263754ad20df2642
JuanDMeGon <juandmegon@gmail.com>
2017-02-16 03:59:09 -0500

    Implementing The Migration for Transactions

--- database/migrations/2017_02_15_060654_create_transactions_table.php
+++ database/migrations/2017_02_15_060654_create_transactions_table.php
@@ -15,7 +15,13 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('quantity')->unsigned();
+            $table->integer('buyer_id')->unsigned();
+            $table->integer('product_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('buyer_id')->references('id')->on('users');
+            $table->foreign('product_id')->references('id')->on('products');
         });
     }
 
