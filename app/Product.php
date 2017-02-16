a72516d9672fc1b85c1f18184d77028fa69f8e3b
JuanDMeGon <juandmegon@gmail.com>
2017-02-16 01:46:33 -0500

    Implementing the properties for Product

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+	const AVAILABLE_PRODUCT = 'available';
+	const UNAVAILABLE_PRODUCT = 'unavailable';
+
+    protected $fillable = [
+    	'name',
+    	'description',
+    	'quantity',
+    	'status',
+    	'image',
+    	'seller_id',
+    ];
+
+    public function isAvailable()
+    {
+    	return $this->status == Product::AVAILABLE_PRODUCT;
+    }
 }
