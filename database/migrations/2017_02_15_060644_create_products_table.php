fd5b1a2fe4fce342ac0f86f4ad2d001a0a75a1c3
JuanDMeGon <juandmegon@gmail.com>
2017-02-16 03:54:31 -0500

    Implementing The Migration for Products

--- database/migrations/2017_02_15_060644_create_products_table.php
+++ database/migrations/2017_02_15_060644_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Product;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -15,7 +16,15 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('description', 1000);
+            $table->integer('quantity')->unsigned();
+            $table->string('status')->default(Product::UNAVAILABLE_PRODUCT);
+            $table->string('image');
+            $table->integer('seller_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('seller_id')->references('id')->on('users');
         });
     }
 
