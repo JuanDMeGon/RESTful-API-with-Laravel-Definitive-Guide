5caf5ad4b3a555ddf35d545cb892f8b0554c13aa
JuanDMeGon <juandmegon@gmail.com>
2017-02-16 22:48:21 -0500

    Creating the Migration for The Pivot Table

---
+++ database/migrations/2017_02_17_034324_category_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CategoryProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->integer('category_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_product');
+    }
+}
