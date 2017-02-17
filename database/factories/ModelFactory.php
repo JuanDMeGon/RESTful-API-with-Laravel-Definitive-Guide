efca530f417d465d400174c9963db57cde694fd6
JuanDMeGon <juandmegon@gmail.com>
2017-02-17 02:49:14 -0500

    Creating The Factory for Transaction

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\User;
+use App\Seller;
 use App\Product;
 use App\Category;
+use App\Transaction;
 
 /*
 |--------------------------------------------------------------------------
@@ -47,6 +49,19 @@ $factory->define(Product::class, function (Faker\Generator $faker) {
         'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
         'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
         'seller_id' => User::all()->random()->id,
+        // User::inRandomOrder()->first()->id
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker\Generator $faker) {
+
+	$seller = Seller::has('products')->get()->random();
+	$buyer = User::all()->except($seller->id)->random();
+
+    return [
+        'quantity' => $faker->numberBetween(1, 3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
         // User::inRandomOrder()->first()->id
     ];
 });
