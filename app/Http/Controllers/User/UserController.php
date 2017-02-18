b1bcc2dfc01f1a20bacac2d087e23884e618d75d
JuanDMeGon <juandmegon@gmail.com>
2017-02-18 01:52:11 -0500

    Implementing The Destroy Method for UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -117,6 +117,10 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        $user->delete();
+
+        return response()->json(['data' => $user], 200);
     }
 }
