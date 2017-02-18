82a9d43d6c5146227df5b7d151db6dbba95201d7
JuanDMeGon <juandmegon@gmail.com>
2017-02-18 01:15:13 -0500

    Implementing The Show Method for UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -40,7 +40,9 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        return response()->json(['data' => $user], 200);
     }
 
     /**
