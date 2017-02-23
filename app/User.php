92ea2b569f8dce5ee3edb0330f6c643324ff5307
JuanDMeGon <juandmegon@gmail.com>
2017-02-23 15:35:56 -0500

    Defining Mutators and Accessor for Models

--- app/User.php
+++ app/User.php
@@ -42,6 +42,22 @@ class User extends Authenticatable
         'verification_token',
     ];
 
+    public function setNameAttribute($name)
+    {
+        $this->attributes['name'] = strtolower($name);
+    }
+
+    public function getNameAttribute($name)
+    {
+        return ucwords($name);
+    }
+
+    public function setEmailAttribute($email)
+    {
+        $this->attributes['email'] = strtolower($email);
+    }
+
+
     public function isVerified()
     {
         return $this->verified == User::VERIFIED_USER;
