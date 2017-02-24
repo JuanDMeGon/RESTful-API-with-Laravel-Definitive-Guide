5d7587fbf659185428588e6cfe5ed36d8f22238b
JuanDMeGon <juandmegon@gmail.com>
2017-02-23 20:18:03 -0500

    Handling AuthenticationException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -57,6 +57,10 @@ class Handler extends ExceptionHandler
 
             return $this->errorResponse("Does not exists any {$modelName} with the specified identificator", 404);
         }
+
+        if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $exception);
+        }
         return parent::render($request, $exception);
     }
 
@@ -69,11 +73,7 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-
-        return redirect()->guest('login');
+        return $this->errorResponse('Unauthenticated.', 401);
     }
 
     /**
