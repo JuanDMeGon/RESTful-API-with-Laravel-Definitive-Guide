eae03f05ee46dcc3521406d45a76843a9d4618ff
JuanDMeGon <juandmegon@gmail.com>
2017-02-24 00:14:52 -0500

    Resolving Seller Using Global Scopes

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -26,10 +26,8 @@ class SellerController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Seller $seller)
     {
-        $seller = Seller::has('products')->findOrFail($id);
-
         return $this->showOne($seller);
     }
 }
--- app/Seller.php
+++ app/Seller.php
@@ -3,9 +3,17 @@
 namespace App;
 
 use App\Product;
+use App\Scopes\SellerScope;
 
 class Seller extends User
 {
+	protected static function boot()
+	{
+		parent::boot();
+
+		static::addGlobalScope(new SellerScope);
+	}
+
     public function products()
     {
     	return $this->hasMany(Product::class);
