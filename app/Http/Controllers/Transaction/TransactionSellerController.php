8db9bd121f8c5f44dc9d06ea43576eb798e6b806
JuanDMeGon <juandmegon@gmail.com>
2017-02-25 00:43:24 -0500

    Implementing the Index Method for TransactionSellerController

---
+++ app/Http/Controllers/Transaction/TransactionSellerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Transaction;
+
+use App\Transaction;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class TransactionSellerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Transaction $transaction)
+    {
+        $seller = $transaction->product->seller;
+
+        return $this->showOne($seller);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,7 @@ Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show
  */
 Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
 Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' => ['index']]);
+Route::resource('transactions.sellers', 'Transaction\TransactionSellerController', ['only' => ['index']]);
 
 /**
  * Users
