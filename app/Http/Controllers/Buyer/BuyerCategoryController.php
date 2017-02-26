d93f39bbe2ad680ac392fb3cab2ac82635f1669c
JuanDMeGon <juandmegon@gmail.com>
2017-02-26 01:21:25 -0500

    Implementing the Index Method for BuyerCategoryController

---
+++ app/Http/Controllers/Buyer/BuyerCategoryController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class BuyerCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        $sellers = $buyer->transactions()->with('product.categories')
+            ->get()
+            ->pluck('product.categories')
+            ->collapse()
+            ->unique('id')
+            ->values();
+
+        return $this->showAll($sellers);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Http\Request;
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
 Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['index']]);
 Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
+Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' => ['index']]);
 Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
 
 /**
