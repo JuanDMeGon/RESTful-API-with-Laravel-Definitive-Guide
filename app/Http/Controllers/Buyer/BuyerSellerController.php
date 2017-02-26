98cfbb872d6ca619e5534426c43e94d1d0c8d7a1
JuanDMeGon <juandmegon@gmail.com>
2017-02-26 01:12:05 -0500

    Implementing the Index Method for BuyerSellerController

---
+++ app/Http/Controllers/Buyer/BuyerSellerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class BuyerSellerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        $sellers = $buyer->transactions()->with('product.seller')
+            ->get()
+            ->pluck('product.seller')
+            ->unique('id')
+            ->values();
+
+        return $this->showAll($sellers);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Http\Request;
  * Buyers
  */
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['index']]);
 Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
 Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
 
