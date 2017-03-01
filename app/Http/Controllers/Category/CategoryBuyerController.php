870bf85c9e99a505b49e9b4aaeb1671ea2d551a3
JuanDMeGon <juandmegon@gmail.com>
2017-03-01 00:48:34 -0500

    Implementing the Index Method for CategoryBuyerController

---
+++ app/Http/Controllers/Category/CategoryBuyerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class CategoryBuyerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Category $category)
+    {
+        $buyers = $category->products()
+            ->whereHas('transactions')
+            ->with('transactions.buyer')
+            ->get()
+            ->pluck('transactions')
+            ->collapse()
+            ->pluck('buyer')
+            ->unique('id')
+            ->values();
+
+        return $this->showAll($buyers);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,7 @@ Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['onl
  * Categories
  */
 Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+Route::resource('categories.buyers', 'Category\CategoryBuyerController', ['only' => ['index']]);
 Route::resource('categories.sellers', 'Category\CategorySellerController', ['only' => ['index']]);
 Route::resource('categories.products', 'Category\CategoryProductController', ['only' => ['index']]);
 Route::resource('categories.transactions', 'Category\CategoryTransactionController', ['only' => ['index']]);
