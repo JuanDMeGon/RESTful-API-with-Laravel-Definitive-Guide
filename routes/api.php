4d7bbdbd1381256f10442d095170456861b3fd08
JuanDMeGon <juandmegon@gmail.com>
2017-03-01 01:05:10 -0500

    Implementing the Index Method for SellerTransactionController

--- routes/api.php
+++ routes/api.php
@@ -40,6 +40,7 @@ Route::resource('products', 'Product\ProductController', ['only' => ['index', 's
  * Sellers
  */
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index', 'show']]);
 
 /**
  * Transactions
