07119d101ec470f67586dc36693524a0da825da8
JuanDMeGon <juandmegon@gmail.com>
2017-03-01 01:25:54 -0500

    Implementing the Index Method for SellerBuyerController

---
+++ app/Http/Controllers/Seller/SellerBuyerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Seller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class SellerBuyerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Seller $seller)
+    {
+        $buyers = $seller->products()
+            ->whereHas('transactions')
+            ->with('transactions.buyer')
+            ->get()
+            ->pluck('transactions')
+            ->collapse()
+            ->pluck('buyer')
+            ->unique('id')
+            ->values();
+
+        return $this->showAll($buyers);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -40,8 +40,9 @@ Route::resource('products', 'Product\ProductController', ['only' => ['index', 's
  * Sellers
  */
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
-Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index', 'show']]);
-Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index', 'show']]);
+Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
+Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
+Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
 
 /**
  * Transactions
