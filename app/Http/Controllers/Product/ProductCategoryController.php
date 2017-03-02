6fe3ada95b2b34605c77921ff8dd5c2651f6287e
JuanDMeGon <juandmegon@gmail.com>
2017-03-02 01:52:39 -0500

    [Challenge] Implementing the Index Operation of ProductCategory

---
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class ProductCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Product $product)
+    {
+        $categories = $product->categories;
+
+        return $this->showAll($categories);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -36,6 +36,7 @@ Route::resource('categories.transactions', 'Category\CategoryTransactionControll
  */
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
 Route::resource('products.buyers', 'Product\ProductBuyerController', ['only' => ['index']]);
+Route::resource('products.categories', 'Product\ProductCategoryController', ['only' => ['index', 'update', 'destroy']]);
 Route::resource('products.transactions', 'Product\ProductTransactionController', ['only' => ['index']]);
 
 /**
