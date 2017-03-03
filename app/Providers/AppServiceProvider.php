5a8940c52568bc8e05ec47d50c8d804b2554b2da
JuanDMeGon <juandmegon@gmail.com>
2017-03-03 02:18:10 -0500

    Handling the Products Availability Using Events

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Product;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +16,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        Product::updated(function($product) {
+            if ($product->quantity == 0 && $product->isAvailable()) {
+                $product->status = Product::UNAVAILABLE_PRODUCT;
+
+                $product->save();
+            }
+        });
     }
 
     /**
