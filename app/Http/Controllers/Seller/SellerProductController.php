c5bae4ab3e4180c0ef1a4dbd458779cd5a175124
JuanDMeGon <juandmegon@gmail.com>
2017-03-03 20:10:28 -0500

    Storing an Image When Creating a Product

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -43,7 +43,7 @@ class SellerProductController extends ApiController
         $data = $request->all();
 
         $data['status'] = Product::UNAVAILABLE_PRODUCT;
-        $data['image'] = '1.jpg';
+        $data['image'] = $request->image->store('');
         $data['seller_id'] = $seller->id;
 
         $product = Product::create($data);
