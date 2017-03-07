b39f9536e5ae2498e91834e071df2b150e223e66
JuanDMeGon <juandmegon@gmail.com>
2017-03-06 23:26:39 -0500

    Preparing the Project for Data Transforming

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Spatie\Fractal\FractalServiceProvider::class,
 
         /*
          * Application Service Providers...
