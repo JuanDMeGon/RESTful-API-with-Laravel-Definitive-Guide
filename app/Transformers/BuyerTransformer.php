d9c8c8a7b569151bbfd727bcb53f62f8a99ba7a3
JuanDMeGon <juandmegon@gmail.com>
2017-03-07 14:47:18 -0500

    Creating the Transformer for Seller and Buyer

---
+++ app/Transformers/BuyerTransformer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Buyer;
+use League\Fractal\TransformerAbstract;
+
+class BuyerTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Buyer $buyer)
+    {
+        return [
+            'identifier' => (int)$buyer->id,
+            'name' => (string)$buyer->name,
+            'email' => (string)$buyer->email,
+            'isVerified' => (int)$buyer->verified,
+            'creationDate' => $buyer->created_at,
+            'lastChange' => $buyer->updated_at,
+            'deletedDate' => isset($buyer->deleted_at) ? (string) $buyer->deleted_at : null,
+        ];
+    }
+}
---
+++ app/Transformers/SellerTransformer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+
+class SellerTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Seller $seller)
+    {
+        return [
+            'identifier' => (int)$seller->id,
+            'name' => (string)$seller->name,
+            'email' => (string)$seller->email,
+            'isVerified' => (int)$seller->verified,
+            'creationDate' => $seller->created_at,
+            'lastChange' => $seller->updated_at,
+            'deletedDate' => isset($seller->deleted_at) ? (string) $seller->deleted_at : null,
+        ];
+    }
+}
