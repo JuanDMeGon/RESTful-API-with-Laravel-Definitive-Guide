16e12f139a2d9094dcc1f59ea0c55f4bcb19f0df
JuanDMeGon <juandmegon@gmail.com>
2017-03-07 15:07:14 -0500

    Creating the Transformer for Product

---
+++ app/Transformers/ProductTransformer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Product;
+use League\Fractal\TransformerAbstract;
+
+class ProductTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Product $product)
+    {
+        return [
+            'identifier' => (int)$product->id,
+            'title' => (string)$product->name,
+            'details' => (string)$product->description,
+            'stock' => (int)$product->quantity,
+            'situation' => (string)$product->status,
+            'picture' => url("img/{$product->image}"),
+            'seller' => (int)$product->seller_id,
+            'creationDate' => $product->created_at,
+            'lastChange' => $product->updated_at,
+            'deletedDate' => isset($product->deleted_at) ? (string) $product->deleted_at : null,
+        ];
+    }
+}
