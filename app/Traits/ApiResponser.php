f00048cdabe785bde67494c94712464d1992be86
JuanDMeGon <juandmegon@gmail.com>
2017-03-08 23:47:30 -0500

    Implementing the Solution Using Transformers

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -25,7 +25,7 @@ trait ApiResponser
 
 		$transformer = $collection->first()->transformer;
 
-		$collection = $this->sortData($collection);
+		$collection = $this->sortData($collection, $transformer);
 		$collection = $this->transformData($collection, $transformer);
 
 		return $this->successResponse($collection, $code);
@@ -45,10 +45,10 @@ trait ApiResponser
 		return $this->successResponse(['data' => $message], $code);
 	}
 
-	protected function sortData(Collection $collection)
+	protected function sortData(Collection $collection, $transformer)
 	{
 		if (request()->has('sort_by')) {
-			$attribute = request()->sort_by;
+			$attribute = $transformer::originalAttribute(request()->sort_by);
 
 			$collection = $collection->sortBy->{$attribute};
 		}
