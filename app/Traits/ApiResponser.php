2dd13a05f4e1e262081f728952cba301d11036df
JuanDMeGon <juandmegon@gmail.com>
2017-03-08 23:37:31 -0500

    Sorting Results by Any Attribute

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -25,6 +25,7 @@ trait ApiResponser
 
 		$transformer = $collection->first()->transformer;
 
+		$collection = $this->sortData($collection);
 		$collection = $this->transformData($collection, $transformer);
 
 		return $this->successResponse($collection, $code);
@@ -42,6 +43,17 @@ trait ApiResponser
 	protected function showMessage($message, $code = 200)
 	{
 		return $this->successResponse(['data' => $message], $code);
+	}
+
+	protected function sortData(Collection $collection)
+	{
+		if (request()->has('sort_by')) {
+			$attribute = request()->sort_by;
+
+			$collection = $collection->sortBy->{$attribute};
+		}
+
+		return $collection;
 	}
 
 	protected function transformData($data, $transformer)
