4e49d8a0ad61149893e60d9079227c49d7b219b5
JuanDMeGon <juandmegon@gmail.com>
2017-03-10 00:04:52 -0500

    Filtering Responses Based on Attributes

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -25,6 +25,7 @@ trait ApiResponser
 
 		$transformer = $collection->first()->transformer;
 
+		$collection = $this->filterData($collection, $transformer);
 		$collection = $this->sortData($collection, $transformer);
 		$collection = $this->transformData($collection, $transformer);
 
@@ -43,6 +44,19 @@ trait ApiResponser
 	protected function showMessage($message, $code = 200)
 	{
 		return $this->successResponse(['data' => $message], $code);
+	}
+
+	protected function filterData(Collection $collection, $transformer)
+	{
+		foreach (request()->query() as $query => $value) {
+			$attribute = $transformer::originalAttribute($query);
+
+			if (isset($attribute, $value)) {
+				$collection = $collection->where($attribute, $value);
+			}
+		}
+
+		return $collection;
 	}
 
 	protected function sortData(Collection $collection, $transformer)
