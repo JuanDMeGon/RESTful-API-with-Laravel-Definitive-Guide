424df7c953cf4e97f9756b766b6adadc1942b132
JuanDMeGon <juandmegon@gmail.com>
2017-03-10 00:23:14 -0500

    Allowing Custom Page Size

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -4,6 +4,7 @@ namespace App\Traits;
 
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 trait ApiResponser
@@ -74,9 +75,18 @@ trait ApiResponser
 
 	protected function paginate(Collection $collection)
 	{
+		$rules = [
+			'per_page' => 'integer|min:2|max:50',
+		];
+
+		Validator::validate(request()->all(), $rules);
+
 		$page = LengthAwarePaginator::resolveCurrentPage();
 
 		$perPage = 15;
+		if (request()->has('per_page')) {
+			$perPage = (int) request()->per_page;
+		}
 
 		$results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
 
