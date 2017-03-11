178bc416a80b9376e1d34d01b2a47d368882f8ee
JuanDMeGon <juandmegon@gmail.com>
2017-03-10 20:16:32 -0500

    Implementing HATEOAS Hypermedia Controls for Category Instances

--- app/Transformers/CategoryTransformer.php
+++ app/Transformers/CategoryTransformer.php
@@ -21,6 +21,29 @@ class CategoryTransformer extends TransformerAbstract
             'creationDate' => (string)$category->created_at,
             'lastChange' => (string)$category->updated_at,
             'deletedDate' => isset($category->deleted_at) ? (string) $category->deleted_at : null,
+
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('categories.show', $category->id),
+                ],
+                [
+                    'rel' => 'category.buyers',
+                    'href' => route('categories.buyers.index', $category->id),
+                ],
+                [
+                    'rel' => 'category.products',
+                    'href' => route('categories.products.index', $category->id),
+                ],
+                [
+                    'rel' => 'category.sellers',
+                    'href' => route('categories.sellers.index', $category->id),
+                ],
+                [
+                    'rel' => 'category.transactions',
+                    'href' => route('categories.transactions.index', $category->id),
+                ],
+            ]
         ];
     }
 
