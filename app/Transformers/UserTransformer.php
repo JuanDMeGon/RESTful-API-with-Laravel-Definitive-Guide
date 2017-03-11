345730bc4f0882589e0fc40934c58fbdd45ffe63
JuanDMeGon <juandmegon@gmail.com>
2017-03-10 20:41:14 -0500

    Implementing HATEOAS for User Instances

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -23,6 +23,13 @@ class UserTransformer extends TransformerAbstract
             'creationDate' => (string)$user->created_at,
             'lastChange' => (string)$user->updated_at,
             'deletedDate' => isset($user->deleted_at) ? (string) $user->deleted_at : null,
+
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('users.show', $user->id),
+                ],
+            ]
         ];
     }
 
