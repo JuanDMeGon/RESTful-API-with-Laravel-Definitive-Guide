d88080c420546f4e86c744a9dd6a7f63b22763b6
JuanDMeGon <juandmegon@gmail.com>
2017-03-10 20:45:11 -0500

    [Exercise] Implementing HATEOAS for Seller

--- app/Transformers/SellerTransformer.php
+++ app/Transformers/SellerTransformer.php
@@ -22,6 +22,33 @@ class SellerTransformer extends TransformerAbstract
             'creationDate' => (string)$seller->created_at,
             'lastChange' => (string)$seller->updated_at,
             'deletedDate' => isset($seller->deleted_at) ? (string) $seller->deleted_at : null,
+
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('sellers.show', $seller->id),
+                ],
+                [
+                    'rel' => 'seller.buyers',
+                    'href' => route('sellers.buyers.index', $seller->id),
+                ],
+                [
+                    'rel' => 'seller.categories',
+                    'href' => route('sellers.categories.index', $seller->id),
+                ],
+                [
+                    'rel' => 'seller.products',
+                    'href' => route('sellers.products.index', $seller->id),
+                ],
+                [
+                    'rel' => 'seller.transactions',
+                    'href' => route('sellers.transactions.index', $seller->id),
+                ],
+                [
+                    'rel' => 'user',
+                    'href' => route('users.show', $seller->id),
+                ],
+            ],
         ];
     }
 
