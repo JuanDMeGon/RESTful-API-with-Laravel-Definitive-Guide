3533bb74de41301e27e0810440b78399e5cb83f3
JuanDMeGon <juandmegon@gmail.com>
2017-03-12 01:09:22 -0500

    Tuning Some Structures to Avoid Inconsistences

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,6 @@ $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
 $this->post('login', 'Auth\LoginController@login');
 $this->post('logout', 'Auth\LoginController@logout')->name('logout');
 
-// Registration Routes...
-$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-$this->post('register', 'Auth\RegisterController@register');
-
 // Password Reset Routes...
 $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
@@ -27,3 +23,7 @@ $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 $this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/', function() {
+	return view('welcome');
+})->middleware('guest');
