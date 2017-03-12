75501c458a7fe8c0f65e66bcf3ce1f2aefb19f48
JuanDMeGon <juandmegon@gmail.com>
2017-03-12 01:37:36 -0500

    Returning HTML and Redirections When Required

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -112,6 +112,10 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
+        if ($this->isFrontend($request)) {
+            return redirect()->guest('login');
+        }
+
         return $this->errorResponse('Unauthenticated.', 401);
     }
 
@@ -125,7 +129,19 @@ class Handler extends ExceptionHandler
     protected function convertValidationExceptionToResponse(ValidationException $e, $request)
     {
         $errors = $e->validator->errors()->getMessages();
+
+        if ($this->isFrontend($request)) {
+            return $request->ajax() ? response()->json($error, 422) : redirect()
+                ->back()
+                ->withInput($request->input())
+                ->withErrors($errors);
+        }
         
         return $this->errorResponse($errors, 422);
+    }
+
+    private function isFrontend($request)
+    {
+        return $request->acceptsHtml() && collect($request->route()->middleware())->contains('web');
     }
 }
