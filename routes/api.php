8c1bd0d1474f13f4e7adc7dbee78362aeb9965df
JuanDMeGon <juandmegon@gmail.com>
2017-03-12 20:46:18 -0500

    Registering the Required Routes

--- routes/api.php
+++ routes/api.php
@@ -62,3 +62,5 @@ Route::resource('transactions.sellers', 'Transaction\TransactionSellerController
 Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
 Route::name('verify')->get('users/verify/{token}', 'User\UserController@verify');
 Route::name('resend')->get('users/{user}/resend', 'User\UserController@resend');
+
+Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
