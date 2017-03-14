8ed30f8e749738c34b71e3327eb36271b47a1b52
JuanDMeGon <juandmegon@gmail.com>
2017-03-14 01:50:44 -0500

    Preparing the API to Use the Passport Components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,7 +13,11 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+// Vue.component('example', require('./components/Example.vue'));
+
+//testing
+
+//sdcfdg
 
 const app = new Vue({
     el: '#app'
